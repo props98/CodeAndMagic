567464942c26f577ff72f016143338adef9650ee
htmlMax <my@mail.ru>
2022-07-21 19:35:10 +0300

    Добавлены константы и переменные

--- js/stat.js
+++ js/stat.js
@@ -1,8 +1,32 @@
 "use strict";
-/*
 
-ctx — канвас на котором рисуется игра.
-names — массив, с именами игроков прошедших уровень. Имя самого игрока — Вы. Массив имён формируется случайным образом.
-times — массив, по длине совпадающий с массивом names. Массив содержит время прохождения уровня соответствующего игрока из массива names. Время прохождения уровня задано в миллисекундах.
+(function() {
 
-*/
+    //* Размеры и координаты облака
+    const cloudWidth = 420;
+    const cloudHeight = 270;
+    const cloudBgColor = 'white';
+    const cloudTextColor = 'black';
+    const cloudTextStyle = '16px PT Mono';
+    const cloudTextLineHeight = 20;
+    const cloudPositionX = 100;
+    const cloudPositionY = 10;
+
+    //* Гистограмма времен участников
+    let histogramHeigt = 150;
+    let histogramWidth = 40;
+    let histogramGap = 50;
+    let histogramPlayerColor = 'rgba(255, 0, 0, 1)';
+
+    //* Массив игроков
+    let names = ['Вы', 'Марк', 'Питер', 'Джон'];
+
+    //* Отрисовка облака с результатми игроков
+    window.renderStatistics = function(ctx, names, times) {
+
+        
+
+    }
+
+
+})();
