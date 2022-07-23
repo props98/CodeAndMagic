c57297478307a51eb1eab1bb23ec636f009ba71e
htmlMax <my@mail.ru>
2022-07-23 16:12:13 +0300

    Добавлен скрипт для отрисовки облака - гистограмма

--- js/stat.js
+++ js/stat.js
@@ -3,14 +3,13 @@
 (function() {
 
     //* Размеры и координаты облака
-    const cloudWidth = 420;
-    const cloudHeight = 270;
-    const cloudBgColor = 'white';
-    const cloudTextColor = 'black';
-    const cloudTextStyle = '16px PT Mono';
-    const cloudTextLineHeight = 20;
-    const cloudPositionX = 100;
-    const cloudPositionY = 10;
+    const CLOUD_WIDTH = 420;
+    const CLOUD_HEIGHT = 270;
+    const CLOUD_BG = '#fff';
+    const CLOUD_TEXT_COLOR = 'black';
+    const CLOUD_TEXT_FONT = '16px PT Mono';
+    const CLOUD_POSITION_X = 100;
+    const CLOUD_POSITION_Y = 10;
 
     //* Гистограмма времен участников
     let histogramHeigt = 150;
@@ -31,20 +30,25 @@
     function findInArr(arr) {
         let randArr = arr[0];
         for (let i = 0; i < arr.length; i++) {
-            console.log(arr[i]);
             if (randArr < arr[i]) {
                 randArr = arr[i];
             }
         }
-
         return randArr;
     }
 
+    //* Функция для создания облака
+    let renderCloud = function(ctx, x, y, color) {
+        ctx.fillStyle = color;
+        ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+    }
 
     //* Отрисовка облака с результатми игроков
     window.renderStatistics = function(ctx, names, times) {
-
-
+        //* Рисуем тень от облака со смещением в права и вниз
+        renderCloud(ctx, CLOUD_POSITION_X + 10, CLOUD_POSITION_Y + 10, 'rgba(0, 0, 0, 0.3)');
+        //* Рисуем основное облако
+        renderCloud(ctx, CLOUD_POSITION_X, CLOUD_POSITION_Y, CLOUD_BG);
 
     }
 
